3520e99b6834ddac6729f409300f7623eca87770
Dhairya Surana <dhairs7@vt.edu>
2020-04-07 20:36:50 -0400

    Wrote a function to get cells based on string

--- fit_grapher.py
+++ fit_grapher.py
@@ -5,19 +5,27 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("Fitness Log").sheet1
+def getData(sheet_name):
 
-# Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
 
-#print("ahsdf")
-#criteria = re.compile(r'chest press')
-#print(sheet.findall(criteria))
+    # Find a workbook by name and open the first sheet
+    # Make sure you use the right name here.
+    sheet = client.open(sheet_name).sheet1
+
+    return sheet
+
+def getCells(sheet, criteria):
+    
+    criteria = re.compile(r'chest press')
+    return sheet.findall(criteria)
+
+if __name__ == "__main__":
+
+    sheet = getData("Fitness Log")
+    print(sheet.get_all_records())
+    print(getCells(sheet, "chest press"))
